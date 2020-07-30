7bf796e928eea03ebb9647379176ec984dbd1573
DMLooter <verbanmike@gmail.com>
2020-07-30 16:30:51 -0500

    Made Albums resizable and List-backed like Playlists

--- src/main/java/com/verban/media/Album.java
+++ src/main/java/com/verban/media/Album.java
@@ -10,19 +10,23 @@ public class Album extends Media {
 
 	private static final long serialVersionUID=19834754;
 
-	private Song[] tracks;
+	private List<Song> tracks;
 
 	/**
 	* Creates a new empty album with the specified title, year, and artists, and room for the specified number of songs.
 	* Fils the tracks list with the PLACEHOLDER song.
 	* @param title the name of this Album
-	* @param tracks the number of songs on this Album
+	* @param tracks the expected number of songs on this Album
 	* @param year the year this Album was published
 	* @param artists the artists that contributed to this Album
 	*/
 	public Album(String title, int tracks, int year, String artist){
-		this(title, new Song[tracks], year, artist);
-		Arrays.fill(this. tracks, Song.PLACEHOLDER);
+		this.title = title;
+		this.tracks = new ArrayList<Song>();
+		for(int i =0; i < tracks; i ++)
+			this.tracks.add(Song.PLACEHOLDER);
+		this.year = year;
+		this.artist = artist;
 	}
 
 	/**
@@ -34,7 +38,8 @@ public class Album extends Media {
 	*/
 	public Album(String title, Song[] tracks, int year, String artist){
 		this.title = title;
-		this.tracks = tracks;
+		this.tracks = new ArrayList<Song>();
+		this.tracks.addAll(tracks);
 		this.year = year;
 		this.artist = artist;
 	}
@@ -45,23 +50,25 @@ public class Album extends Media {
 	* @return the nth track, or null.
 	*/
 	public Song getTrack(int n){
-		if(n < 1 || n > tracks.length)
+		if(n < 1 || n > tracks.size())
 			return null;
-		return tracks[n-1];
+		return tracks.get(n-1);
 	}
 
 	/**
 	* Sets the nth track on this Album to the specified song.
-	* Fails if n is outside the range of tracks on this album.
+	* If n is 0, or > tracks.size(), the song will be added to the end.
 	* @param s the song to put into this Album
 	* @param n the track number to set to s, 1-INDEXED!
-	* @return true if n is a valid track number and the set succeded, false otherwise.
+	* @return true if n is a valid track number and the set succeded, false if the song was added to the end.
 	*/
 	public boolean setTrack(Song s, int n){
-		if(n < 1 || n > tracks.length)
+		if(n < 1 || n > tracks.size()){
+			tracks.add(s);
 			return false;
+		}
 
-		tracks[n-1] = s;
+		tracks.set(n-1, s);
 		return true;
 	}
 
@@ -70,22 +77,22 @@ public class Album extends Media {
 	* @return a copy of the list of tracks in this Album
 	*/
 	public Song[] getAllTracks(){
-		return Arrays.copyOf(tracks, tracks.length);
+		return tracks.toArray(new Song[0]);
 	}
 
 	/**
 	* Returns the number of tracks on this album.
 	*/
 	public int numTracks(){
-		return tracks.length;
+		return tracks.size();
 	}
 
 	@Override
 	public boolean equals(Object o){
 		if(o instanceof Album){
 			Album a = (Album)o;
-			// Two equal albums will have the same name, year, and number of tracks.
-			return a.title.equals(this.title) && a.year == this.year && a.tracks.length == this.tracks.length;
+			// Two equal albums will have the same title, year, and artist.
+			return a.title.equals(this.title) && a.year == this.year;
 		}
 		return false;
 	}
